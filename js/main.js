fd008d52161debcf66e3cbfd63d47b60b96af1b1
Ivona Pepic <ivona.pepic@gmail.com>
2022-06-24 10:27:17 +0200

    remove obstacles when they gou oustside

--- js/main.js
+++ js/main.js
@@ -19,6 +19,14 @@ class Game {
             // move all obstacles
             this.obstacleArr.forEach((obstacleInstance) => {
                 obstacleInstance.moveDown();
+
+
+            // remove obstacles  when they reach the bottom
+                if((obstacleInstance.positionY + obstacleInstance.height) === 0){
+                    this.obstacleArr.shift();               //remove from array
+                    obstacleInstance.domElement.remove();   //remove from the dom (screen)
+                }
+
             });
 
             // detect collision
@@ -54,7 +62,7 @@ class Player {
     constructor(){
         this.positionX = 45;
         this.positionY = 0;
-        this.height = 20;
+        this.height = 10;
         this.width = 10;
 
         this.domElement = this.createDomElement();
@@ -90,7 +98,7 @@ class Player {
 
 class Obstacle {
     constructor(){
-        this.width = 10;
+        this.width =  this.positionX = Math.floor(Math.random() * (30 - 0)) + 0; //meke obstacle size random
         this.height = 10;
         this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // generate random number between 0 and (100-width)        
         this.positionY = 90;
